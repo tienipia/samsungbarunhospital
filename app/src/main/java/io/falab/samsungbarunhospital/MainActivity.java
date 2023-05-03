a806e1e6983d39e0de4dac5b1950a323eaaca66f
James Choi <tienipia@gmail.com>
2023-05-03 17:25:58 +0900

    Init

---
+++ app/src/main/java/io/falab/samsungbarunhospital/MainActivity.java
@@ -0,0 +1,163 @@
+package io.falab.samsungbarunhospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper mViewFlipper;
+    private ImageView[] imageButtons;
+
+    private Button button;
+
+
+    private long last_clicked = 0L;
+
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageButtons = new ImageView[5];
+
+        imageButtons[0] = findViewById(R.id.imageButton1);
+        imageButtons[1] = findViewById(R.id.imageButton2);
+        imageButtons[2] = findViewById(R.id.imageButton3);
+        imageButtons[3] = findViewById(R.id.imageButton4);
+        imageButtons[4] = findViewById(R.id.imageButton5);
+
+        imageButtons[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewActivity(MainActivity.this, "com.google.android.youtube");
+            }
+        });
+        imageButtons[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewActivity(MainActivity.this, "kr.co.captv.pooqV2");
+            }
+        });
+        imageButtons[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("http://samsungbarunhospital.com");
+            }
+        });
+        imageButtons[3].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("https://www.naver.com");
+
+            }
+        });
+        imageButtons[4].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openWebPage("https://blog.naver.com/samsungbarunhospital");
+            }
+        });
+
+        button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public synchronized void onClick(View v) {
+                long cur = System.currentTimeMillis();
+
+                if ((cur - last_clicked) < 500) {
+                    counter = counter + 1;
+                } else {
+                    counter = 1;
+                }
+
+                if (counter > 3) {
+                    Log.d("AAA", "SHOW");
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    final EditText input = new EditText(MainActivity.this);
+                    input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    builder.setView(input);
+                    builder.setTitle("관리자 모드");
+                    builder.setMessage("관리자 비밀번호를 입력해주세요.");
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("확인",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if ("1872".equals(input.getText().toString())) {
+                                        Toast.makeText(getApplicationContext(), "관리자 진입", Toast.LENGTH_SHORT).show();
+                                        startNewActivity(MainActivity.this, "com.simplemobiletools.applauncher");
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+                    builder.setNegativeButton("취소",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    last_clicked = 0;
+                                    counter = 0;
+                                }
+                            });
+                    builder.show();
+                } else {
+                    last_clicked = cur;
+                }
+            }
+        });
+
+        mViewFlipper = findViewById(R.id.view_flipper);
+
+        Animation inAnim = new AlphaAnimation(0f, 1f);
+        inAnim.setDuration(300);
+        mViewFlipper.setInAnimation(inAnim);
+
+        Animation outAnim = new AlphaAnimation(1f, 0f);
+        outAnim.setDuration(300);
+        mViewFlipper.setOutAnimation(outAnim);
+
+        mViewFlipper.startFlipping();
+        mViewFlipper.setFlipInterval(60000);
+    }
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void startNewActivity(Context context, String packageName) {
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        if (intent != null) {
+            // We found the activity now start the activity
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        } else {
+            // Bring user to the market or let them choose an app?
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.parse("market://details?id=" + packageName));
+            context.startActivity(intent);
+        }
+    }
+}
