9dec7c6136eaf1188c5855d08772ca9ee51681c5
James Choi <tienipia@gmail.com>
2023-05-10 16:23:17 +0900

    Support left right button

--- app/src/main/java/io/falab/samsungbarunhospital/MainActivity.java
+++ app/src/main/java/io/falab/samsungbarunhospital/MainActivity.java
@@ -25,11 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView[] imageButtons;
 
-    private Button button;
+    private Button buttonLeft;
 
-    private long last_clicked = 0L;
+    private Button buttonRight;
 
-    private int counter = 0;
+    private long last_clicked_left = 0L;
+
+    private long last_clicked_right = 0L;
+
+    private int counterLeft = 0;
+
+    private int counterRight = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,20 +84,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button = findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonLeft = findViewById(R.id.btnLeft);
+        buttonLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public synchronized void onClick(View v) {
                 long cur = System.currentTimeMillis();
 
-                if ((cur - last_clicked) < 500) {
-                    counter = counter + 1;
+                if ((cur - last_clicked_left) < 500) {
+                    counterLeft = counterLeft + 1;
                 } else {
-                    counter = 1;
+                    counterLeft = 1;
                 }
 
-                if (counter > 3) {
-                    Log.d("AAA", "SHOW");
+                if (counterLeft > 3) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                     final EditText input = new EditText(MainActivity.this);
                     input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_PASSWORD);
@@ -103,8 +108,10 @@ public class MainActivity extends AppCompatActivity {
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
                                     if ("1872".equals(input.getText().toString())) {
-                                        Toast.makeText(getApplicationContext(), "관리자 진입", Toast.LENGTH_SHORT).show();
-                                        startNewActivity(MainActivity.this, "com.simplemobiletools.applauncher");
+                                        Toast.makeText(getApplicationContext(), "설정 진입", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
                                     } else {
                                         Toast.makeText(getApplicationContext(), "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show();
                                     }
@@ -114,14 +121,61 @@ public class MainActivity extends AppCompatActivity {
                     builder.setNegativeButton("취소",
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
-                                    last_clicked = 0;
-                                    counter = 0;
+                                    last_clicked_left = 0;
+                                    counterLeft = 0;
                                 }
                             });
                     builder.show();
                 } else {
-                    last_clicked = cur;
+                    last_clicked_left = cur;
                 }
+            }
+        });
+
+
+        buttonRight = findViewById(R.id.btnRight);
+        buttonRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public synchronized void onClick(View v) {
+                long cur = System.currentTimeMillis();
+
+                if ((cur - last_clicked_right) < 500) {
+                    counterRight = counterRight + 1;
+                } else {
+                    counterRight = 1;
+                }
+
+                if (counterRight > 3) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    final EditText input = new EditText(MainActivity.this);
+                    input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    builder.setView(input);
+                    builder.setTitle("런처 실행");
+                    builder.setMessage("관리자 비밀번호를 입력해주세요.");
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("확인",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if ("1872".equals(input.getText().toString())) {
+                                        startNewActivity(MainActivity.this, "com.android.launcher3");
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+                    builder.setNegativeButton("취소",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    last_clicked_right = 0;
+                                    counterRight = 0;
+                                }
+                            });
+                    builder.show();
+                } else {
+                    last_clicked_right = cur;
+                }
+
             }
         });
 
